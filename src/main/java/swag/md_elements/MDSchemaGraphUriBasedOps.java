34f8ba2431d5388325d9d327799ae9ec322ddf2d
swag-bi <swag-bi@users.noreply.github.com>
2022-04-22 10:29:18 +0200

    ds

---
+++ src/main/java/swag/md_elements/MDSchemaGraphUriBasedOps.java
@@ -0,0 +1,111 @@
+package swag.md_elements;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MDSchemaGraphUriBasedOps extends MDSchemaGraphQB4OLAP {
+
+	public MDSchemaGraphUriBasedOps(String name, String uri, String nameSpace, String endpoint,
+			String preferredLabelProperty, MDSchemaType type) {
+		super(name, uri, nameSpace, endpoint, preferredLabelProperty, type);
+	}
+
+	public List<MDRelation> getPath(String startLevel, String endLevel, String dimension) {
+
+		List<MDRelation> path = new LinkedList<>();
+
+		for (MDElement elem : this.getMdGraphMap().keySet()) {
+			if (elem.getURI().equals(startLevel) && elem instanceof Level && getDimensionsOfLevel(startLevel).stream()
+					.map(d -> d.getURI()).collect(Collectors.toSet()).contains(dimension)) {
+
+			}
+		}
+
+		return path;
+	}
+
+	public MDRelation getRollUpProperty(String l1, String d1, String l2, String d2) {
+
+		for (MDElement elem1 : getAllElemsWithUri(l1)) {
+			if (elem1 instanceof Level) {
+				if (deosOutEdgesTargetsContainElem(getOutEdgesOfNode(elem1), d1)) {
+					for (MDElement elem2 : getAllElemsWithUri(l2)) {
+						if (elem1 instanceof Level) {
+							if (deosOutEdgesTargetsContainElem(getOutEdgesOfNode(elem2), d2)) {
+								for (MDRelation rel : getMdGraphMap().values().stream().flatMap(val -> val.stream())
+										.collect(Collectors.toSet())) {
+									if (rel instanceof QB4OHierarchyStep && rel.getSource().getURI().equals(l1)
+											&& rel.getTarget().getURI().equals(l2)) {
+										return rel;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean deosOutEdgesTargetsContainElem(Set<MDRelation> rels, String elm) {
+
+		for (MDRelation rel : rels) {
+			if (rel.getTarget().getURI().equals(elm)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Set<MDRelation> getOutEdgesOfNode(MDElement e) {
+		Set<MDRelation> rels = new HashSet<>();
+
+		for (MDRelation rel : this.getAllEdges()) {
+			if (rel.getSource().getURI().equals(e.getURI())) {
+				rels.add(rel);
+			}
+		}
+		return rels;
+	}
+
+	public Set<MDElement> getAllElemsWithUri(String uri) {
+		Set<MDElement> elems = new HashSet<>();
+
+		for (MDElement elem : getMdGraphMap().keySet()) {
+			if (elem.getURI().equals(uri)) {
+				elems.add(elem);
+			}
+		}
+		return elems;
+	}
+
+	public Set<Dimension> getDimensionsOfLevel(Level l) {
+
+		Set<Dimension> dimensions = new HashSet<>();
+
+		Set<Level> levels = getLevelByNonUNiqueUri(l.getURI());
+
+		for (Level level : levels) {
+			if (level != null && level instanceof Level) {
+				Set<MDRelation> rels = getOutEdgesOfNode(level);
+
+				for (MDRelation rel : rels) {
+
+					if (rel instanceof InDimension) {
+
+						if (rel.getTo() != null && rel.getTo() instanceof Dimension) {
+							dimensions.add((Dimension) rel.getTo());
+						}
+					}
+				}
+			}
+		}
+
+		return dimensions;
+	}
+
+}
--- src/main/java/swag/sparql_builder/QueryUtils.java
+++ src/main/java/swag/sparql_builder/QueryUtils.java
@@ -2,15 +2,11 @@ package swag.sparql_builder;
 
 import java.util.List;
 
-import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.syntax.TripleCollectorBGP;
 
-import com.ibm.icu.util.Measure;
-
-import swag.analysis_graphs.execution_engine.analysis_situations.LevelInAnalysisSituation;
 import swag.graph.Path;
 import swag.md_elements.Descriptor;
 import swag.md_elements.Dimension;
@@ -19,69 +15,64 @@ import swag.md_elements.Level;
 import swag.md_elements.MDElement;
 import swag.md_elements.MDRelation;
 import swag.md_elements.MDSchemaGraphQB4OLAP;
-import swag.md_elements.MappingFunctions;
 
 public class QueryUtils {
-	
+
 	MDSchemaGraphQB4OLAP schema;
 
 	public Var getVarOfLevel(Dimension d, Level l) {
-		return getVarOfLevel(d.getName(), l.getName());		
+		return getVarOfLevel(d.getName(), l.getName());
 	}
-	
+
 	public Var getVarOfLevel(String d, String l) {
 		String name = d + "_" + l;
 		return Var.alloc(name);
 	}
-	
+
 	public Var getVarOfLevelAttribute(Dimension d, Level l, Descriptor a) {
 		String name = d.getName() + "_" + l.getName() + "_" + a.getName();
 		return Var.alloc(name);
 	}
-	
+
 	public Var getVarOfMeasure(swag.md_elements.Measure m) {
 		String name = m.getName();
 		return Var.alloc(name);
 	}
-	
+
 	public Var getVarOfFact(Fact f) {
 		String name = f.getName();
 		return Var.alloc(name);
 	}
-	
+
 	public TripleCollectorBGP getTriplesOfLevel(String d, String l) {
-		
-		TripleCollectorBGP bgp = new TripleCollectorBGP();			
-		Triple triple = new Triple(getVarOfLevel(d,l),
-				NodeFactory.createURI("http://purl.org/qb4olap/cubes#memberOf"),
+
+		TripleCollectorBGP bgp = new TripleCollectorBGP();
+		Triple triple = new Triple(getVarOfLevel(d, l), NodeFactory.createURI("http://purl.org/qb4olap/cubes#memberOf"),
 				NodeFactory.createURI(l));
 		bgp.addTriple(triple);
 		return bgp;
 	}
-	
+
 	public TripleCollectorBGP getTriplesOfRollUp(String d, String l, String d2, String l2) {
-		
-		TripleCollectorBGP bgp = new TripleCollectorBGP();			
-		Triple triple1 = new Triple(getVarOfLevel(d,l),
-				NodeFactory.createURI("http://purl.org/qb4olap/cubes#memberOf"),
-				NodeFactory.createURI(l));
+
+		TripleCollectorBGP bgp = new TripleCollectorBGP();
+		Triple triple1 = new Triple(getVarOfLevel(d, l),
+				NodeFactory.createURI("http://purl.org/qb4olap/cubes#memberOf"), NodeFactory.createURI(l));
 		bgp.addTriple(triple1);
 		MDRelation rel = schema.getRollUpProperty(d, l, d2, l2);
-		Triple triple2 = new Triple(getVarOfLevel(d,l),
-				NodeFactory.createURI(rel.getURI()),
+		Triple triple2 = new Triple(getVarOfLevel(d, l), NodeFactory.createURI(rel.getURI()),
 				NodeFactory.createURI(l2));
 		bgp.addTriple(triple2);
 		return bgp;
 	}
-	
+
 	public TripleCollectorBGP getTriplesOfPath(String d, String l, String l2) {
-		TripleCollectorBGP bgp = new TripleCollectorBGP();			
-		
+		TripleCollectorBGP bgp = new TripleCollectorBGP();
+
 		List<Path<MDElement, MDRelation>> list;
 
 		list = schema.getAllMappedPathsBetweenTwoVertices(startElement, endElement);
-		
-		
+
 		return bgp;
 	}
 }
